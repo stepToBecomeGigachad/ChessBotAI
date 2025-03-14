bb8eceaf4852ab5abd402d01eb985876f79c2447
n0n4m3_404 <167999420+Crabs536@users.noreply.github.com>
2025-03-14 23:07:21 +0700

    Update checkmate and stalemate

--- Chess_engine.py
+++ Chess_engine.py
@@ -18,12 +18,20 @@ class GameState():
         self.moveFunctions= {'P': self.getPawnMoves, 'R': self.getRockMoves, 'N': self.getKnightMoves, 'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
         self.white_to_move = True
         self.move_log = []
+        self.white_king_location = (7, 4)
+        self.black_king_location = (0, 4)
+        self.checkMate = False
+        self.staleMate = False
 
     def makeMove(self, move):
         self.board[move.startRow][move.startCol] = "--"
         self.board[move.endRow][move.endCol] = move.pieceMoved
         self.move_log.append(move)
         self.white_to_move = not self.white_to_move
+        if move.pieceMoved == "wK":
+            self.white_king_location = (move.endRow, move.endCol)
+        elif move.pieceMoved == "bK":
+            self.black_king_location = (move.endRow, move.endCol)
 
     # Undo the last move
     def undoMove(self):
@@ -32,13 +40,59 @@ class GameState():
             self.board[move.startRow][move.startCol] = move.pieceMoved
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.white_to_move = not self.white_to_move #switch turns back
+            # update the king's position needed
+            if move.pieceMoved == "wK":
+                self.white_king_location = (move.startRow, move.startRow)
+            elif move.pieceMoved == "bK":
+                self.black_king_location = (move.startRow, move.startRow)
 
     # All moves considering checks
 
     def getValidMoves(self):
-        pass
-        return self.getAllPosibleMoves() # for now we will not worry about checks
+        # 1.) generate all possible moves
+        moves = self.getAllPosibleMoves()
+        # 2.) for each move , make the move
+        for i in range(len(moves) - 1, -1, -1):  # when removing from a list go backwards through that list
+            self.makeMove(moves[i])
+            # 3.) generate all opponent's moves
+            # 4.) for each of your opponent's moves, see if they attack your king
+            self.white_to_move = not self.white_to_move
+            if self.inCheck():
+                moves.remove(moves[i])  # 5.) if they do attack your king, not a valid move
+            self.white_to_move = not self.white_to_move
+            self.undoMove()
+        # print("Valid moves: ", [move.getChessNotation() for move in moves])  # Debug
+        if len(moves) == 0:
+            if self.inCheck():
+                self.checkMate = True
+                print("Checkmate")
+            else:
+                self.staleMate = True
+                print("Stalemate")
+        else:
+            self.checkMate = False
+            self.staleMate = False
 
+        return moves
+        
+     # Determine if the current player is in check
+    def inCheck(self):
+
+        if self.white_to_move:
+            return self.squaredUnderAttack(self.white_king_location[0], self.white_king_location[1])
+        else:
+            return self.squaredUnderAttack(self.black_king_location[0], self.black_king_location[1])
+
+    # Determine if the enemy can attack the square r,c
+    def squaredUnderAttack(self, r, c):
+        self.white_to_move = not self.white_to_move  # switch to opponent's move
+        oopMoves = self.getAllPosibleMoves()
+        self.white_to_move = not self.white_to_move  # switch turn back
+        for move in oopMoves:
+            if move.endRow == r and move.endCol == c:  # square is under attack
+                return True
+        return False
+    
     # All moves without considering checks
     def getAllPosibleMoves(self):
         moves = []
