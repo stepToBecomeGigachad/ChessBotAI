6cc13f9d18655264710f4c62542f7a5c715c8521
n0n4m3_404 <167999420+Crabs536@users.noreply.github.com>
2025-05-07 15:41:09 +0700

    Using Greedy Algorithm and MinMax without recursion for AI_move

--- Chess_AI.py
+++ Chess_AI.py
@@ -1,12 +1,76 @@
 import random
+import Chess_engine
 
-
-
+pieceScore = {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "P": 1}
+CHECKMATE = 1000
+STALEMATE = 0
 
 def findRandomMoves(validMoves):
     if len(validMoves) > 0:
         return validMoves[random.randint(0, len(validMoves)-1)]
     return None
 
-def findBestMove():
-    return
+def findBestMove(gs, validMoves):
+    turnMultiplier = 1 if gs.white_to_move else -1
+    opponentMinMaxScore = CHECKMATE
+    bestPlayerMove = None
+    random.shuffle(validMoves)
+    
+    for playerMove in validMoves:
+        gs.makeMove(playerMove)
+        opponentMoves = gs.getValidMoves()
+        if gs.stalemate:
+            opponentMaxScore = STALEMATE
+        elif gs.checkmate:
+            opponentMaxScore = -CHECKMATE
+        else:
+            opponentMaxScore = -CHECKMATE
+        
+        for opponentMove in opponentMoves:
+            gs.makeMove(opponentMove)
+            gs.getValidMoves()
+            if gs.checkmate:
+                score = CHECKMATE
+            elif gs.stalemate:
+                score = STALEMATE
+            else:
+                score = -turnMultiplier * scoreMaterial(gs.board)
+            if score > opponentMaxScore:
+                opponentMaxScore = score
+                
+            gs.undoMove()
+            
+        if opponentMaxScore < opponentMinMaxScore:
+            opponentMinMaxScore = opponentMaxScore
+            bestPlayerMove = playerMove
+        gs.undoMove()
+    
+    return bestPlayerMove
+
+# def findBestMove(gs, validMoves): # Minimax algorithm
+#     turnMultiplier = 1 if gs.white_to_move else -1
+#     maxScore = -CHECKMATE
+#     bestMove = None
+#     for playerMove in validMoves:
+#         gs.makeMove(playerMove)
+#         if gs.checkmate:
+#             score = CHECKMATE
+#         elif gs.stalemate:
+#             score = STALEMATE
+#         else:
+#             score = scoreMaterial(gs.board) * turnMultiplier
+#         if score > maxScore:
+#             maxScore = score
+#             bestMove = playerMove
+#         gs.undoMove()
+#     return bestMove
+
+def scoreMaterial(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += pieceScore[square[1]]
+            elif square[0] == 'b':
+                score -= pieceScore[square[1]]
+    return score
